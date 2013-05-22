4a28355501636c37567814744f7883684a749dbb
Kasper Vervaecke <kaspervervaecke@gmail.com>
2013-05-22 15:06:43 +0200

    implemented SmallestIncrease, either this or NearestNeighbor might be incorrect, task notes are somewhat confusing

--- src/main/java/gna/SmallestIncreaseTour.java
+++ src/main/java/gna/SmallestIncreaseTour.java
@@ -11,11 +11,15 @@ public class SmallestIncreaseTour extends IncrementallyConstructedTour {
     public SmallestIncreaseTour(World world) {
         super(world);
         tour = new ArrayList<Point>();
+        constructTour();
     }
 
     @Override
     public void insert(Point point) {
-        throw new RuntimeException("not implemented");
+        if (points == null) {
+            points = new ArrayList<Point>();
+        }
+        points.add(point);
     }
 
     @Override
@@ -32,5 +36,32 @@ public class SmallestIncreaseTour extends IncrementallyConstructedTour {
         return tour;
     }
 
-    private List<Point> tour;
+    private void constructTour() {
+        tour = new ArrayList<Point>();
+        List<Point> unvisitedPoints = new ArrayList<Point>(points);
+        Point initialPoint = unvisitedPoints.remove(0);
+        for (int i = 0; i < points.size(); i++) {
+            tour.add(initialPoint);
+            initialPoint = findClosest(initialPoint, unvisitedPoints);
+            unvisitedPoints.remove(initialPoint);
+        }
+    }
+
+    private Point findClosest(Point initialPoint, List<Point> unvisitedPoints) {;
+        Point closest;
+        if (unvisitedPoints.size() != 0) {
+            closest = unvisitedPoints.get(0);
+            for (Point next : unvisitedPoints) {
+                if (next.distanceTo(initialPoint) < closest.distanceTo(initialPoint)
+                        && next != initialPoint) {
+                    closest = next;
+                }
+            }
+        } else {
+            closest = points.get(0);
+        }
+        return closest;
+    }
+
+    private List<Point> tour, points;
 }
